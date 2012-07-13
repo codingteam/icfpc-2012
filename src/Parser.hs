30ef9d512aa25af7f002df1307bbf1a49a330b31
Ilya Portnov <portnov@iportnov.ru>
2012-07-13 22:25:42 +0600

    Implement simplest input parser.

---
+++ src/Parser.hs
@@ -0,0 +1,28 @@
+
+module Parser where
+
+import Lifter
+
+forEachLine :: (String -> IO a) -> IO [a]
+forEachLine fn = go []
+  where
+    go acc = do
+      line <- getLine
+      if null line
+        then return acc
+        else do
+             res <- fn line
+             next <- go acc
+             return (res: next)
+
+readInput :: IO MineState
+readInput = forEachLine (return . map cell)
+  where
+    cell ch = case ch of ' ' -> Empty
+                         '.' -> Earth
+                         '#' -> Wall
+                         '*' -> Rock
+                         '\\' -> Lambda
+                         'R' -> Robot
+                         'L' -> ClosedLift
+                         other -> error ("Unknown character: " ++ [other])
