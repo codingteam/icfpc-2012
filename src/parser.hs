6dd8bf203e68808862a928c88a8f133212432e9d
hagane <deemson@gmail.com>
2012-07-13 20:58:37 +0600

    reading should work now

--- src/parser.hs
+++ src/parser.hs
@@ -2,7 +2,7 @@ module Parser where
 
 import Prelude
 
-data Cell = Empty | Earth | Wall | Rock | Lambda | Robot | ClosedLift | OpenLift deriving Show
+data Cell = Empty | Earth | Wall | Rock | Lambda | Robot | ClosedLift | OpenLift deriving (Show,Eq)
 
 cell ch = case ch of ' ' -> Empty
                      '.' -> Earth
@@ -12,3 +12,16 @@ cell ch = case ch of ' ' -> Empty
                      'R' -> Robot
                      'L' -> ClosedLift
                      other -> error ("Unknown character: " ++ [other])
+
+parseRow row = map cell row
+
+readRow () =
+    do
+      chars <- getLine
+      return (parseRow chars)
+
+bound row = all (\ c -> c == Wall || c == ClosedLift) row
+
+--Don't know if just reading endlessly will work, but i guess it will read only what it'll need
+readMap () =
+    (readRow ()) : (readMap ())
