cf71826d20bcadafe28322620649734b536a1ded
rexim <reximkut@gmail.com>
2012-07-14 00:27:41 +0700

    Reimplemented parser

--- src/Parser.hs
+++ src/Parser.hs
@@ -1,41 +1,23 @@
 
 module Parser where
 
-import System.IO
-
 import Lifter
 
-forEachLine :: (Int -> String -> IO a) -> IO [a]
-forEachLine fn = go [] 0
-  where
-    go acc lineLength = do
-      eof <- hIsEOF stdin
-      if eof
-        then return acc
-        else do
-             line <- getLine
-             let n = if lineLength == 0
-                       then length line
-                       else lineLength
-             if null line
-               then return acc
-               else do
-                    res <- fn n line
-                    next <- go acc n
-                    return (res: next)
+charToCell :: Char -> Cell
+charToCell ch = case ch of ' ' -> Empty
+                           '.' -> Earth
+                           '#' -> Wall
+                           '*' -> Rock
+                           '\\' -> Lambda
+                           'R' -> Robot
+                           'L' -> ClosedLift
+                           other -> error ("Unknown character: " ++ [other])
+
+padLine size line = line ++ replicate n ' '
+  where n = max 0 (length line - size)
 
 readInput :: IO MineState
-readInput = forEachLine $ \n line -> return (map cell $ pad n line)
-  where
-    pad n str
-        | length str >= n = str
-        | otherwise       = str ++ replicate (n - length str) ' '
-
-    cell ch = case ch of ' ' -> Empty
-                         '.' -> Earth
-                         '#' -> Wall
-                         '*' -> Rock
-                         '\\' -> Lambda
-                         'R' -> Robot
-                         'L' -> ClosedLift
-                         other -> error ("Unknown character: " ++ [other])
+readInput = do inputLines <- fmap lines getContents
+               let maxLength = maximum $ map length inputLines
+               let paddedLines = map (padLine maxLength) inputLines
+               return $ map (map charToCell) paddedLines
