9dd2338c0d45bb3c29a8793805b69e010b8adcd1
ForNeVeR <neverthness@gmail.com>
2012-07-13 22:14:45 +0700

    Emulator: generalized tests.

--- src/EmulatorTest.hs
+++ src/EmulatorTest.hs
@@ -5,11 +5,14 @@ module Main (
 import Emulator (emulate)
 import Lifter
 
+type TestCase = (MineState, Action)
+
+test :: TestCase -> MineState -> IO()
+test (field, action) expected =
+    let (field', _, _) = emulate field action 0
+    in  putStrLn $ "Real:     " ++ (show field') ++ "\n"
+                ++ "Expected: " ++ (show expected)
+
 main :: IO()
 main = do
-    putStrLn $ show $ test1 ()
-
-test1 () =
-    let field = [[Robot, Lambda]]
-        action = ARight
-    in  emulate field action 0
+    test ([[Robot, Lambda]], ARight) [[Empty, Robot]]
