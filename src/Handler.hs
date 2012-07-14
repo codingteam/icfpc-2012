9a23e5dc67bb87c3718f7511a403bfba23e86405
Ilya Portnov <portnov@iportnov.ru>
2012-07-14 14:52:08 +0600

    Signal handler works.

--- src/Handler.hs
+++ src/Handler.hs
@@ -1,22 +1,31 @@
 
 module Handler where
 
+import Control.Concurrent
 import Control.Concurrent.STM
 import System.Posix.Signals
 import System.Exit
 
 import Lifter
 
+type ExitLock = TMVar ExitCode
+
 handleSignals :: a -> (TVar a -> IO b) -> (a -> IO ()) -> IO ()
 handleSignals def fn output = do
   var <- atomically $ newTVar def
-  installHandler sigINT (Catch $ handler var output) Nothing
-  fn var
+  mvar <- atomically $ newEmptyTMVar
+  installHandler sigINT (Catch $ handler mvar var output) Nothing
+  forkIO (fn var >> atomically (putTMVar mvar ExitSuccess))
+  exitOnSignal mvar
   result <- atomically $ readTVar var
   output result
 
-handler :: TVar a -> (a -> IO ()) -> IO ()
-handler var output = do
+exitOnSignal :: ExitLock -> IO ()
+exitOnSignal var = atomically (takeTMVar var) >>= exitWith
+
+handler :: ExitLock -> TVar a -> (a -> IO ()) -> IO ()
+handler mvar var output = do
   result <- atomically $ readTVar var
   output result
-  exitWith ExitSuccess
+  atomically $ putTMVar mvar ExitSuccess
+
