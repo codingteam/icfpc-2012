2358d507271cce1ad201b3c78003b9b052be5483
ForNeVeR <neverthness@gmail.com>
2012-07-14 10:48:42 +0700

    Added sizeX and sizeY functions for mine measurement.

--- src/Lifter.hs
+++ src/Lifter.hs
@@ -2,7 +2,8 @@ module Lifter (
     Action   (..),
     Cell     (..),
     GameState(..),
-    MineState
+    MineState,
+    sizeX, sizeY
 ) where
 
 data Cell = Empty | Earth | Wall | Rock | Lambda | Robot | ClosedLift | OpenLift
@@ -19,3 +20,10 @@ data GameState = GameState {
     gmMineState :: MineState,
     gmFinished  :: Bool
 } deriving (Eq, Show)
+
+sizeX :: MineState -> Int
+sizeX field = let row = field !! 0
+              in  length row
+
+sizeY :: MineState -> Int
+sizeY field = length field
