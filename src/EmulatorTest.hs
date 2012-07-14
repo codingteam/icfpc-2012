bbbfbfc58cad846105a0c567181d948b36e397e1
ForNeVeR <neverthness@gmail.com>
2012-07-14 12:19:00 +0700

    Emulator: test suite improved.

--- src/EmulatorTest.hs
+++ src/EmulatorTest.hs
@@ -7,7 +7,7 @@ import Lifter
 
 type TestCase = (GameState, Action)
 
-test :: Int -> TestCase -> GameState -> IO()
+test :: Show a => a -> TestCase -> GameState -> IO()
 test n (state, action) expected =
     let result = emulate state action
     in  if result == expected
@@ -16,7 +16,7 @@ test n (state, action) expected =
                 putStrLn $ " Real:     " ++ (show result)
                 putStrLn $ " Expected: " ++ (show expected)
 
-doTest :: Int -> MineState -> Int -> Int -> Action
+doTest :: Show a => a -> MineState -> Int -> Int -> Action
        -> MineState -> Int -> Int -> Bool -> IO()
 doTest n field lambdas score action field' lambdas' score' finished =
     test n (GameState { gmMineState = field,
@@ -30,11 +30,54 @@ doTest n field lambdas score action field' lambdas' score' finished =
 
 main :: IO()
 main = do
-    doTest 1 [[Robot, Lambda]] 0 0 ARight
-             [[Empty, Robot]] 1 24 False
-    doTest 2 [[Robot, Lambda, ClosedLift]] 0 0 ARight
-             [[Empty, Robot, OpenLift]] 1 24 False
-    doTest 3 [[Robot]] 1 24 AAbort
-             [[Robot]] 1 49 True
-    doTest 4 [[Robot, OpenLift]] 1 0 ARight
-             [[Empty, Robot]] 1 49 True
+    -- Movement:
+    doTest "up"
+        [[Empty], [Robot]] 0 0 AUp
+        [[Robot], [Empty]] 0 (-1) False
+    doTest "down"
+        [[Robot], [Empty]] 0 0 ADown
+        [[Empty], [Robot]] 0 (-1) False
+    doTest "left"
+        [[Empty, Robot]] 0 0 ALeft
+        [[Robot, Empty]] 0 (-1) False
+    doTest "right"
+        [[Robot, Empty]] 0 0 ARight
+        [[Empty, Robot]] 0 (-1) False
+
+    -- TODO: Trying to move into an obstacle.
+    -- TODO: Trying to move into map egde.
+
+    -- Digging:
+    doTest "dig"
+        [[Robot, Earth]] 0 0 ARight
+        [[Empty, Robot]] 0 (-1) False
+
+    -- TODO: Moving rocks.
+    -- TODO: Immovable rocks.
+
+    -- Taking lambdas:
+    doTest "take"
+        [[Robot, Lambda]] 0 0 ARight
+        [[Empty, Robot]] 1 24 False
+
+    -- Opening lifts:
+    doTest "open"
+        [[Robot, Lambda, ClosedLift]] 0 0 ARight
+        [[Empty, Robot, OpenLift]] 1 24 False
+
+    -- TODO: Falling rocks.
+    -- TODO: Sliding rocks from a lambdas.
+    -- TODO: Destroying rocks with collision.
+    -- TODO: Dying under the falling rock.
+    -- TODO: Not dying under the stable rock.
+
+    -- Aborting the game:
+    doTest "abort"
+        [[Robot]] 1 24 AAbort
+        [[Robot]] 1 49 True
+    -- TODO: Aborting the game in the pre-death conditions.
+
+    -- Winning the game:
+    doTest "win"
+        [[Robot, OpenLift]] 1 0 ARight
+        [[Empty, Robot]] 1 49 True
