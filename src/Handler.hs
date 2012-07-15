050e79bc196fb8ef47e929de6df6005423b54faf
Ilya Portnov <portnov@iportnov.ru>
2012-07-15 08:44:22 +0600

    Fix bug in Handler.hs.
    output was called twice under some conditions.

--- src/Handler.hs
+++ src/Handler.hs
@@ -1,6 +1,7 @@
 
 module Handler where
 
+import Control.Monad (when)
 import Control.Concurrent
 import Control.Concurrent.STM
 import System.Posix.Signals
@@ -8,22 +9,22 @@ import System.Exit
 
 import Core
 
-type ExitLock = TMVar ExitCode
+type ExitLock = TMVar Bool
 
 handleSignals :: a -> (TVar a -> IO b) -> (a -> IO ()) -> IO ()
 handleSignals def fn output = do
   var <- atomically $ newTVar def
   mvar <- atomically $ newEmptyTMVar
   installHandler sigINT (Catch $ handler mvar var output) Nothing
-  forkIO (fn var >> atomically (putTMVar mvar ExitSuccess))
-  exitCode <- atomically $ takeTMVar mvar
+  forkIO (fn var >> atomically (putTMVar mvar False))
+  wasSignalHandled <- atomically $ takeTMVar mvar
   result <- atomically $ readTVar var
-  output result
-  exitWith exitCode
+  when (not wasSignalHandled) $ output result
+  exitWith ExitSuccess
 
 handler :: ExitLock -> TVar a -> (a -> IO ()) -> IO ()
 handler mvar var output = do
   result <- atomically $ readTVar var
   output result
-  atomically $ putTMVar mvar ExitSuccess
+  atomically $ putTMVar mvar True
 
